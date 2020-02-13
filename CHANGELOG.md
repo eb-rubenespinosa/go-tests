486b7a249f70dc038bbcf2a203c3e2ddca224e71
runner <runner@Mac-1384.local>
2020-02-13 17:33:38 +0000

    Release 1.1.15

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,9 @@
 
 ## 1.1.14 (2020-02-13)
 sdfg
+
+## 1.1.15 (2020-02-13)
+This are the files changed:
+* yak.rb
+* CHANGELOG.md
+* README.md
--- yak.rb
+++ yak.rb
@@ -2,9 +2,9 @@
 class Yak < Formula
   desc "A command line tool to manage dev environments on Kubernetes"
   homepage "https://github.com/eventbrite/yak"
-  url "https://api.github.com/repos/eb-rubenespinosa/go-tests/releases/assets/18010122"
+  url "https://api.github.com/repos/eb-rubenespinosa/go-tests/releases/assets/18010252"
   sha256 ""
-  version "1.1.14"
+  version "1.1.15"
   bottle :unneeded
   depends_on "kubernetes-cli"
   depends_on "watch"
