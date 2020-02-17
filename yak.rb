c58cd29d7d4a7869d0ea99e724b20ce1126d0182
runner <runner@Mac-1324.local>
2020-02-17 15:47:24 +0000

    Release 1.1.34

--- yak.rb
+++ yak.rb
@@ -2,9 +2,9 @@
 class Yak < Formula
   desc "A command line tool to manage dev environments on Kubernetes"
   homepage "https://github.com/eventbrite/yak"
-  url "https://api.github.com/repos/eb-rubenespinosa/go-tests/releases/assets/18095139"
+  url "https://api.github.com/repos/eb-rubenespinosa/go-tests/releases/assets/18095166"
   sha256 ""
-  version "1.1.33"
+  version "1.1.34"
   bottle :unneeded
   depends_on "kubernetes-cli"
   depends_on "watch"
