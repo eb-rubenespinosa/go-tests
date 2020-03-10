ee84a21c1d5bb29f8b32bd192dbbf684971b8a28
Ruben Espinosa <ruben.espinosa@eventbrite.com>
2020-03-10 12:33:04 +0100

    Add vault-aws-iam-authenticator rb

--- yak.rb
+++ yak.rb
@@ -9,20 +9,15 @@ class Yak < Formula
   bottle :unneeded
   depends_on "kubernetes-cli"
   depends_on "watch"
+  depends_on "vault-aws-iam-authenticator"
 
   resource "aws-iam-authenticator" do
     url "https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/darwin/amd64/aws-iam-authenticator"
     sha256 "ef60d7ea5108b4de19f7fe59514643a7665df6832f94be620405138d8f60dd18"
   end
 
-  resource "vault-aws-iam-authenticator" do
-    url "https://jenkins.evbhome.com/job/vault-aws-iam-authenticator-release/lastSuccessfulBuild/artifact/src/github.com/eventbrite/vault-aws-iam-authenticator/vault-aws-iam-authenticator-darwin-amd64-0.0.6.tar.gz"
-    sha256 "1ed235cbab1fea887dba7ad9383005030d8a6250b61072cb1c343a147715f33f"
-  end
-
   def install
     bin.install "yak"
     resource("aws-iam-authenticator").stage { bin.install "aws-iam-authenticator" }
-    resource("vault-aws-iam-authenticator").stage { bin.install "vault-aws-iam-authenticator" }
   end
 end
